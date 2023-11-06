865fc7711da998076b1934191a27f1112589aa51
SirGooseTheNaughty <vorobyishkaa@mail.ru>
2023-11-06 11:45:05 +0400

    task-7 add import file error handling

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://vao5a9t35e.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://6qw1gwv50a.execute-api.eu-west-1.amazonaws.com",
   bff: "https://i20elx5698.execute-api.eu-west-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
