f2659feec488cc2a62de1f43df30c81da5c641db
SirGooseTheNaughty <vorobyishkaa@mail.ru>
2023-11-09 19:37:43 +0400

    task-8 connect to cart api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://5olen75txg.execute-api.eu-west-1.amazonaws.com",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://i20elx5698.execute-api.eu-west-1.amazonaws.com",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://5olen75txg.execute-api.eu-west-1.amazonaws.com",
 };
 
 export default API_PATHS;
